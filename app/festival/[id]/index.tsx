005d3349368bd60fb30e3f15723f158a2cff3006
kangbeomsu <kbs21c1987@gmail.com>
2025-12-14 14:48:45 +0900

    feat: festival 디테일 페이지 추가

--- app/festival/[id]/index.tsx
+++ app/festival/[id]/index.tsx
@@ -1,15 +1,42 @@
-import { Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { usePathname } from "expo-router";
+import FestivalDetailHeader from "@/components/headers/FestivalDetailHeader";
 
 const FestivalDetailScreen = () => {
   const pathname = usePathname();
   const contentId = pathname.split("/").pop(); // URL에서 축제 ID 추출
 
+  if (!contentId) {
+    return (
+      <View style={styles.container}>
+        <FestivalDetailHeader />
+        <ScrollView style={styles.scrollView}>
+          <View style={{ padding: 20 }}>
+            <Text>상세 내용이 없습니다.</Text>
+          </View>
+        </ScrollView>
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>FestivalDetailScreen</Text>
+    <View style={styles.container}>
+      <FestivalDetailHeader />
+      {/*<FestivalDetailScreenContainer contentId={contentId} />*/}
     </View>
   );
 };
 
 export default FestivalDetailScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: "relative",
+  },
+
+  scrollView: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+});
